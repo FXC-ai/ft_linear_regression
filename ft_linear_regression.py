e25bc603f6e80160f91dfe1cf1c71f8e82a5c837
Francois-xavier Coindreau <fcoindre@c2r7s5.42lausanne.ch>
2024-06-06 19:18:08 +0200

    fonction de cout

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -3,24 +3,25 @@ import csv
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
-file = open("data.csv", 'r')
-reader = csv.reader(file)
-data = list(reader)
-del(data[0])
+# file = open("data.csv", 'r')
+# reader = csv.reader(file)
+# data = list(reader)
+# del(data[0])
 
-nbr_data = len(data)
+# nbr_data = len(data)
 
-print("Nombre d'observations = {}\n".format(nbr_data))
+# print("Nombre d'observations = {}\n".format(nbr_data))
 
-arr_datas = np.array(data, dtype = 'i')
-print("Données initiales : ")
-print(arr_datas)
-print("\n")
+# arr_datas = np.array(data, dtype = 'i')
+# arr_sorted_datas = arr_datas[arr_datas[:,1].argsort()]
+# print("Données triées par ordre décroissant de kilométrage : ")
+# print(arr_datas)
+# print("\n")
 
-arr_sorted_datas = arr_datas[arr_datas[:,1].argsort()]
-print("Données triées par ordre décroissant de kilométrage : ")
-print(arr_datas)
-print("\n")
+
+arr_sorted_datas = np.array([[10,20,30,40],[25,48,66,88]])
+arr_sorted_datas = arr_sorted_datas.T
+print(arr_sorted_datas)
 
 
 # Calcule pour un kilometrage donné le prix de la voiture
@@ -30,16 +31,12 @@ def estimatePrice (x ,theta0, theta1) :
 # Calcule pour tous le kilometrages donnés le prix estimé
 def calculate_model(arr_mileage, theta0, theta1):
     estimatedPrices = estimatePrice(arr_mileage, theta0, theta1)
-    # print("Prix estimés par le modèle pour theta0 = {} et theta1 = {} : ".format (theta0, theta1))
-    # print(estimatedPrices)
-    # print("\n")
     return estimatedPrices
 
 # Evalue la performance du modele
 def fonction_de_cout (estimatedPrices, observationsPrice) :
     arr_error_square = (estimatedPrices - observationsPrice)**2
     arr_error_square_mean = arr_error_square.mean()
-    # print("Valeur de la fonction de cout = {}".format(arr_error_square_mean))
     return arr_error_square_mean
 
 
@@ -58,29 +55,10 @@ def display_cost_fct (arr_performances, arr_theta0, arr_theta1):
     ax.plot_surface(arr_theta0, arr_theta1, arr_performances, cmap="viridis", edgecolor="green")
     plt.show()
 
-arr_mileage = arr_sorted_datas[:,0]
-arr_observations = arr_sorted_datas[:,1]
-arr_theta0 = np.linspace(9500, 10000, 100)
-arr_theta1 = np.linspace(-0.1, 0., 100)
-
-r = calculate_random_gradient_descente (arr_mileage, arr_observations, arr_theta0, arr_theta1)
-
-print(r)
-
-display_cost_fct(r, arr_theta0, arr_theta1)
-
-exit(13)
-# arr_theta0 = np.linspace(0, 10000, 100)
-# arr_theta1 = np.linspace(-0.5, 0.5, 0.01)
-# performance = arr_theta0 * arr_theta1
-# print(performance)
-
-
-
-
 # Calcule la dérivée de la fonction de cout selon theta0
 def derivative_cost_fct_theta0 (estimatedPrice, observationsPrice) :
     arr_error = (estimatedPrice - observationsPrice)
+    print ("estimatedPrice : {} \n\n oservationPrice : {} \n\n arr_error {} \n". format(estimatedPrice, observationsPrice, arr_error))
     derivative_theta0 = arr_error.mean()
     print("derivée en theta0 = {}".format(derivative_theta0))
     return derivative_theta0
@@ -106,34 +84,35 @@ def display_model(mileage, observationsPrice, estimatedPrice):
     plt.close()
 
 
+arr_mileage = arr_sorted_datas[:,0]
+arr_observations = arr_sorted_datas[:,1]
+# arr_theta0 = np.linspace(9500, 10000, 10)
+# arr_theta1 = np.linspace(-0.1, 0., 10)
 
-learningRate = 0.0000001
+# r = calculate_random_gradient_descente (arr_mileage, arr_observations, arr_theta0, arr_theta1)
+# display_cost_fct(r, arr_theta0, arr_theta1)
+
+learningRate = 1
 theta0 = 0
 theta1 = 0
-mileage = arr_sorted_datas[:,0]
-observationsPrice = arr_sorted_datas[:,1]
 
-display_cost_fct(mileage, observationsPrice)
+arr_estimatedPrice_test = calculate_model(arr_mileage, theta0, theta1)
+
+display_model(arr_mileage, arr_observations, arr_estimatedPrice_test)
 
 
-limit = 0
+
+
+# print(arr_estimatedPrice_test)
+
 count = 0
+limit = 1
 while count < limit :
-    estimatedPrice = calculate_model(mileage, theta0, theta1)
-    display_model(mileage, observationsPrice, estimatedPrice)
-    mean_error = fonction_de_cout(mileage, theta0, theta1, observationsPrice)
-    theta0 = calculate_new_theta(derivative_cost_fct_theta0(estimatedPrice, observationsPrice), learningRate)
-    theta1 = calculate_new_theta(derivative_cost_fct_theta1(mileage, estimatedPrice, observationsPrice), learningRate)
-    print("Nouvelles valeurs de theta0 = {} et theta1 = {}".format(theta0, theta1))
+    arr_estimatedPrice = calculate_model(arr_mileage, theta0, theta1)
+    # display_model(mileage, observationsPrice, estimatedPrice)
+    mean_error = fonction_de_cout(arr_estimatedPrice, arr_observations)
+    theta0 = theta0 + calculate_new_theta(derivative_cost_fct_theta0(arr_estimatedPrice, arr_observations), learningRate)
+    theta1 = theta1 + calculate_new_theta(derivative_cost_fct_theta1(arr_mileage, arr_estimatedPrice, arr_observations), learningRate)
+    print("{} : Fonction de cout = {} \nNouvelles valeurs de theta0 = {} et theta1 = {}\n".format(count, mean_error, theta0, theta1))
     count += 1
-
-
-
-
-# arr_test_theta1 = np.arange(-0.5,0.5,0.005)
-# print(arr_test_theta1)
-
-
-
-
 
