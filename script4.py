b65c5815d3391f7f109f0361aab0dd93cd80943f
FXC-ai <fx.coindreau@gmail.com>
2024-06-07 15:06:01 +0200

    in progress, works with my datas and probably need a normalization of the datas

--- script4.py
+++ script4.py
@@ -33,7 +33,7 @@ def drv_cost_fct_theta1_2 (arr_datas, theta0, theta1):
 	return arr_errors.mean()
 
 def display_cost_fct():
-	fig = plt.figure()
+	fig = plt.figure(1)
 	ax = plt.axes(projection="3d")
 	arr_theta0 = np.linspace(0, 14, 10)
 	arr_theta1 = np.linspace(0, 4, 10)
@@ -44,7 +44,16 @@ def display_cost_fct():
 
 	ax.plot_surface(arr_theta0, arr_theta1, values, cmap="viridis", edgecolor="green")
 	plt.show()
+	plt.close()
 
+
+def display_model(arr_datas, arr_estimatedPrice):
+	fig = plt.figure(2)
+	plt.scatter(arr_datas[:,0], arr_datas[:,1], marker = 'P')
+	plt.plot(arr_datas[:,0], arr_estimatedPrice, c = "green")
+	plt.grid()
+	plt.show()
+	plt.close()
 
 # def display_drv_cost_fct_theta0 ():
 # 	fig = plt.figure()
@@ -53,27 +62,36 @@ def display_cost_fct():
 # 	plt.plot()
 
 arr_datas = np.array([[10,20,30,40],[25,48,66,88]]).T
-theta0 = 7
-theta1 = 4
+theta0 = 0
+theta1 = 0
 
-print(cost_fct(theta0,theta1), cost_fct_2(arr_datas, theta0,theta1))
-print("drv en theta0 : ", drv_cost_fct_theta0 (theta0,theta1), 2*drv_cost_fct_theta0_2 (arr_datas, theta0,theta1))
-print("drv en theta1 : ", drv_cost_fct_theta1 (theta0,theta1), 2*drv_cost_fct_theta1_2 (arr_datas, theta0,theta1))
+# print(cost_fct(theta0,theta1), cost_fct_2(arr_datas, theta0,theta1))
+# print("drv en theta0 : ", drv_cost_fct_theta0 (theta0,theta1), 2*drv_cost_fct_theta0_2 (arr_datas, theta0,theta1))
+# print("drv en theta1 : ", drv_cost_fct_theta1 (theta0,theta1), 2*drv_cost_fct_theta1_2 (arr_datas, theta0,theta1))
 
 
-learningRate = 0.001
+learningRate_theta_0 = -0.1
+learningRate_theta_1 = -0.001
 count = 0
-limit = 0
+limit = 10000
 
 while (count < limit) :
-	print(cost_fct(theta0,theta1), cost_fct_2(arr_datas, theta0,theta1))
+	print("fonction de cout : ", cost_fct(theta0,theta1), cost_fct_2(arr_datas, theta0,theta1))
 	print("drv en theta0 : ", drv_cost_fct_theta0 (theta0,theta1), 2*drv_cost_fct_theta0_2 (arr_datas, theta0,theta1))
 	print("drv en theta1 : ", drv_cost_fct_theta1 (theta0,theta1), 2*drv_cost_fct_theta1_2 (arr_datas, theta0,theta1))
-	theta0 = learningRate * drv_cost_fct_theta1 (theta0,theta1)
-
-
-
+	new_theta0 = theta0 + learningRate_theta_0 * drv_cost_fct_theta0_2 (arr_datas, theta0,theta1)
+	new_theta1 = theta1 + learningRate_theta_1 * drv_cost_fct_theta1_2 (arr_datas, theta0, theta1)
+	theta0 = new_theta0
+	theta1 = new_theta1
+	print("nouveau theta 0 = ", theta0)
+	print("nouveau theta 1 = ", theta1)
+	print("\n")
+	count+=1
 
 
 
 display_cost_fct()
+
+arr_estimatedPrice = estimatePrice(arr_datas[:,0], theta0, theta1)
+display_model(arr_datas, arr_estimatedPrice)
+
